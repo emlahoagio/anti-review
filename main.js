02e52aa2215ace15413c17539ab1f44b8d2f69e4
quando <quandtm0610@gmail.com>
2025-03-05 18:40:53 +0700

    remove js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.querySelector(".popup").style.display = "flex";
-    }, 5000); // Hiện popup sau 5 giây
+    }, 1600); // Hiện popup sau 5 giây
 });
