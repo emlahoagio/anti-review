fe0df1fd0b7169aad98cd6bcd3a7ed6f8a355904
quando <quandtm0610@gmail.com>
2025-03-05 17:25:15 +0700

    popup

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".popup").style.display = "flex";
+    }, 5000); // Hiện popup sau 5 giây
+});
